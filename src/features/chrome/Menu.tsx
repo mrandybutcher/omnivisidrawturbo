247eb83c95961e4322ce66a97c239c720b88d35a
Steve Metcalfe <steve.metcalfe@skybettingandgaming.com>
2020-03-30 15:59:34 +0100

    Stopped clicking the menu breaking the app

--- src/features/chrome/Menu.tsx
+++ src/features/chrome/Menu.tsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import styles from "./Menu.module.scss"
 
+function onNoOp(e: React.MouseEvent<HTMLElement>) {
+    e.preventDefault()
+    e.stopPropagation()
+}
+
 function Menu() {
     return (
         <div className={styles.root}>
-            <div className={styles.dropdown}>
-                <button className={styles.dropbtn}>File &#9662; </button>
+            <div className={styles.dropdown} onClick={onNoOp}>
+                <button className={styles.dropbtn} onClick={onNoOp}>File &#9662; </button>
                 <div className={styles.dropdowncontent}>
-                    <a href="/">New...</a>
-                    <a href="/">Open from</a>
-                    <a href="/">Open recent</a>
-                    <a href="/">Save</a>
-                    <a href="/">Save as</a>
-                    <a href="/">Rename</a>
-                    <a href="/">Make a copy</a>
-                    <a href="/">Import from</a>
-                    <a href="/">Export as</a>
-                    <a href="/">New library</a>
-                    <a href="/">Open library from</a>
-                    <a href="/">Page setup...</a>
-                    <a href="/">Print...</a>
-                    <a href="/">Close</a>
+                    <a href="/" onClick={onNoOp}>New...</a>
+                    <a href="/" onClick={onNoOp}>Open from</a>
+                    <a href="/" onClick={onNoOp}>Open recent</a>
+                    <a href="/" onClick={onNoOp}>Save</a>
+                    <a href="/" onClick={onNoOp}>Save as</a>
+                    <a href="/" onClick={onNoOp}>Rename</a>
+                    <a href="/" onClick={onNoOp}>Make a copy</a>
+                    <a href="/" onClick={onNoOp}>Import from</a>
+                    <a href="/" onClick={onNoOp}>Export as</a>
+                    <a href="/" onClick={onNoOp}>New library</a>
+                    <a href="/" onClick={onNoOp}>Open library from</a>
+                    <a href="/" onClick={onNoOp}>Page setup...</a>
+                    <a href="/" onClick={onNoOp}>Print...</a>
+                    <a href="/" onClick={onNoOp}>Close</a>
                 </div>
             </div>
-            <a href="/">Edit</a>
-            <a href="/">View</a>
-            <a href="/">Extras</a>
-            <a href="/">Help</a>
+            <a href="/" onClick={onNoOp}>Edit</a>
+            <a href="/" onClick={onNoOp}>View</a>
+            <a href="/" onClick={onNoOp}>Extras</a>
+            <a href="/" onClick={onNoOp}>Help</a>
         </div>
     )
 }
